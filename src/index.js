f9057d6ff2efb7305e4a832ebecebd3d32a2219b
Paul Daulby <Paul.Daulby@amnestyint.onmicrosoft.com>
2018-06-27 12:12:12 +0100

    add recursive counterpoints

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import './index.css';
 import {PointBox} from './pointBox';
 const R = require('ramda');
 
-const renderPointBox = (value) => (<PointBox description={value.description} onClick={()=>(alert('not implemented'))}/>);
-const renderAll = R.map(renderPointBox);
+const renderPointBox = (value) => (<PointBox description={value.description} onClick={()=>(alert('not implemented'))}/>);  
+
+const renderPoint = function(point) {
+    return(
+        <div className="point">
+            {renderPointBox(point)}
+            <div className="counterPoint">
+                {R.map(renderPoint, point.counterpoints)}
+            </div>
+        </div>
+    )
+}
+
+const renderAll = R.map(renderPoint);  
 
 class All extends React.Component {
     constructor(props) {
@@ -14,12 +26,19 @@ class All extends React.Component {
         this.state = {
             points: [
                 {
-                    id: 0,
                     description: "x gon give it to ya, something something something sfaksd jflajsdf lakjsdflaks jdflasd jflask lkdalkjalfdkj",
-                    counterpoints: []
+                    counterpoints: [
+                        {
+                            description: "counter counter countersomething something sfaksd jflajsdf lakjsdflaks jdflasd jflask lkdalkjalfdkj",
+                            counterpoints: []
+                        },
+                        {
+                            description: "counter2 counter2 counter2something something sfaksd jflajsdf lakjsdflaks jdflasd jflask lkdalkjalfdkj",
+                            counterpoints: []
+                        },
+                    ]
                 },
                 {
-                    id: 0,
                     description: "y gon give it to ya, something something something sfaksd jflajsdf lakjsdflaks jdflasd jflask lkdalkjalfdkj",
                     counterpoints: []
                 },
